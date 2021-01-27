36d5ed6b3de3e5d5c07e6c6c4a7e043876276d42
tsun-code <61623464+tsun-code@users.noreply.github.com>
2021-01-28 00:21:11 +0800

    Complete the mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,20 +1,30 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
+
 const styleOpen = "open";
 const styleFadeIn = "fade-in";
 const styleFadeOut = "fade-out";
-const header = document.querySelector(".header");
-const overlay = document.querySelector(".overlay");
+const noScrollStyle = "no-scroll";
 
 btnHamburger.addEventListener("click", function() {
     //console.log("open-hamburger");
-    if (header.classList.contains(styleOpen)) {
+    if (header.classList.contains(styleOpen)) {  // Close hamburger menu
         header.classList.remove(styleOpen);
-        overlay.classList.remove(styleFadeIn);
-        overlay.classList.add(styleFadeOut);
+        body.classList.remove(noScrollStyle);
+        fadeElems.forEach(function(element) {
+            element.classList.remove(styleFadeIn);
+            element.classList.add(styleFadeOut); 
+        });
         
-    } else {
+    } else {  // Open hamburger menu
         header.classList.add(styleOpen);
-        overlay.classList.remove(styleFadeOut);
-        overlay.classList.add(styleFadeIn);
+        body.classList.add(noScrollStyle);
+        fadeElems.forEach(function(element) {
+            element.classList.remove(styleFadeOut);
+            element.classList.add(styleFadeIn); 
+        });
     }
 });
