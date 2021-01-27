fb94b4d85d12edb538e62bb31da273f12d3aeb3e
tsun-code <61623464+tsun-code@users.noreply.github.com>
2021-01-27 21:49:40 +0800

    Adding overlay effect

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,20 @@
 const btnHamburger = document.querySelector("#btnHamburger");
-const openMenuStyle = "open";
+const styleOpen = "open";
+const styleFadeIn = "fade-in";
+const styleFadeOut = "fade-out";
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", function() {
     //console.log("open-hamburger");
-    if (header.classList.contains(openMenuStyle)) {
-        header.classList.remove(openMenuStyle);
+    if (header.classList.contains(styleOpen)) {
+        header.classList.remove(styleOpen);
+        overlay.classList.remove(styleFadeIn);
+        overlay.classList.add(styleFadeOut);
+        
     } else {
-        header.classList.add(openMenuStyle);
+        header.classList.add(styleOpen);
+        overlay.classList.remove(styleFadeOut);
+        overlay.classList.add(styleFadeIn);
     }
 });
